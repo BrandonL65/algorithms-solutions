5b713f60c462d6124a67c7ec180a66a4386b889f
Brandon Lau <brandon.lau86@gmail.com>
2020-08-20 03:43:32 -0400

    add answer for monotonic array

--- AE/Array/05-MoveElementToEnd.js
+++ AE/Array/05-MoveElementToEnd.js
@@ -1,5 +1,5 @@
 /*
-
+  Difficulty: Medium
   You're given an array of integers and an integer. Write a function that moves
   all instances of that integer in the array to the end of the array and returns
   the array.
