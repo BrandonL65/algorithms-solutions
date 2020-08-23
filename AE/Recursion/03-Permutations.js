913156e4e130e8ff25789c213336124fa12d98d3
Brandon Lau <brandon.lau86@gmail.com>
2020-08-22 20:41:13 -0400

    add comments and solution

--- AE/Recursion/03-Permutations.js
+++ AE/Recursion/03-Permutations.js
@@ -16,6 +16,11 @@
 Method 1 - recursively 
 Upper Bound: O(n^2*n!) time | O(n*n!) space
 Roughly: O(n*n!) time | O(n*n!) space
+- create permutations [] to hold all permutations generated, plus a helper fn that calculates the permutations 
+- the helper fn accepts the array to pick permutations from, the current generated permutation, and the array of all permutations to push on
+- the base case is if the array's length < 1, and the current permutation array is longer than length 1, we push the current permutation to the all permutations array
+- for each index of array, we pick that as the "next" value to slap onto our permutation, and we generate the leftover array. Then we call the same fn onto the leftover arr.
+- so if we have [1,2,3], we will first pick 1, left with [2,3] and call the fn again on that, then we pick 2, left with [1,3], then 3 with [1,2], etc.
 */
 function getPermutations(array) {
   let permutations = [];
