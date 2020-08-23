662d5f7d5a490fcf8eed31450c98d1b9e6b33227
Brandon Lau <brandon.lau86@gmail.com>
2020-08-23 18:47:46 -0400

    insertion sort finish

--- AE/Sorting/02-InsertionSort.js
+++ AE/Sorting/02-InsertionSort.js
@@ -11,3 +11,26 @@
   Sample Output 
   [2, 3, 5, 5, 6, 8, 9]
 */
+
+/*
+Method 1 - Insertion Sort
+TC: O(n^2)
+SC: O(1)
+- Insertion Sort works by having a loop and a nested loop. For each outer loop, you loop from the outer loop index back to 0, and "insert" the value
+- Therefore, everything behind the current outer loop index is sorted. You "insert"/swap the values of the inner loop until you encounter a value that is less than the inner loop array[idx]
+- Then, you would know that every value after that is smaller. 
+https://visualgo.net/bn/sorting
+*/
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j >= 0 && array[j] < array[j - 1]; j--) {
+      if (array[j] < array[j - 1]) {
+        let prevValue = array[j - 1];
+        array[j - 1] = array[j];
+        array[j] = prevValue;
+      }
+    }
+  }
+  return array;
+}
