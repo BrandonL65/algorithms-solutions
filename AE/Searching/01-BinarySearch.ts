a745593fa5da638df3cfd4286e7634ecda6c3cae
Brandon Lau <brandon.lau86@gmail.com>
2020-08-24 06:50:17 -0400

    add difficulty

--- AE/Searching/01-BinarySearch.ts
+++ AE/Searching/01-BinarySearch.ts
@@ -1,5 +1,5 @@
 /*
-
+  Difficulty: Easy
   Write a function that takes in a sorted array of integers as well as a target
   integer. The function should use the Binary Search algorithm to determine if
   the target integer is contained in the array and should return its index if it
