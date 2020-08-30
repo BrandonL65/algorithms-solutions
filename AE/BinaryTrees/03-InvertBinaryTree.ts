3d1a5b4d5a849a745e64dfa0fbd4c3f10ec24d99
Brandon Lau <brandon.lau86@gmail.com>
2020-08-30 10:07:50 -0400

    invert binary tree done

--- AE/BinaryTrees/03-InvertBinaryTree.ts
+++ AE/BinaryTrees/03-InvertBinaryTree.ts
@@ -29,3 +29,60 @@ Sample Output
             /   \
            9     8
 */
+
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+/*
+Method 1 - Recursively
+TC: O(n)
+SC: O(d) d is depth of tree
+- If tree(node) is null, we return
+- If not null, swap that node's left and right branches 
+- Call the same fn recursively  on node's left and right branches
+*/
+function invertBinaryTree(tree: BinaryTree) {
+  if (!tree) {
+    return;
+  }
+
+  let leftBranch = tree.left;
+  tree.left = tree.right;
+  tree.right = leftBranch;
+}
+
+/*
+Method 2 - Using a queue
+TC: O(n)
+SC: O(n)
+- Have a queue that initially holds the root tree node
+- While queue.length, shift off the first element of the queue
+- Invert that node's left and right branches
+- If left branch exists, push it onto queue
+- Do same for right branch
+*/
+
+function invertBinaryTree2(tree: BinaryTree) {
+  let queue = [tree];
+  while (queue.length) {
+    let currentNode = queue.shift()!;
+    let leftBranch = currentNode.left;
+    currentNode.left = currentNode.right;
+    currentNode.right = leftBranch;
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+}
