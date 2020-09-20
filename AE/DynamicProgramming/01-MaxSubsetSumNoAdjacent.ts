712891bcc3c90c113f4ac8f1c7272fdc83b2fdf3
Brandon Lau <brandon.lau86@gmail.com>
2020-09-20 00:33:27 -0400

    add prompt

---
+++ AE/DynamicProgramming/01-MaxSubsetSumNoAdjacent.ts
@@ -0,0 +1,13 @@
+/*
+  Difficulty: Easy
+
+  Write a function that takes in an array of positive integers and returns the
+  maximum sum of non-adjacent elements in the array.
+  If a sum can't be generated, the function should return 0.
+  
+  Sample Input
+  array = [75, 105, 120, 75, 90, 135]
+
+  Sample Output
+  330, which is 75+120+135
+*/
