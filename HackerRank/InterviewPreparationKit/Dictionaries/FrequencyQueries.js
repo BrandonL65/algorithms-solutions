9c4f3f9fcbae7b9bbf95966b1d8cc951b5269488
Brandon Lau <brandonlau@Brandons-MacBook-Pro.local>
2020-12-21 12:58:20 -0500

    added efficient solution

---
+++ HackerRank/InterviewPreparationKit/Dictionaries/FrequencyQueries.js
@@ -0,0 +1,5 @@
+/*
+Interview Preparation Kit - Dictionary - Frequency Queries
+Difficulty: Medium 
+https://www.hackerrank.com/challenges/frequency-queries/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=dictionaries-hashmaps
+*/
